3223b92388f7ba809aa31f60d4166e86abd3ded3
Athari <athargul07@gmail.com>
2024-03-05 13:37:03 +0500

    added readme.md

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 import json
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+lines = """---------------------------------------------
+---------------------------------------------
+---------------------------------------------"""
 
 with open("chapters.json","r") as f:
     data = json.loads(f.read())
     chapters = data['chapters']
     total_verses = 0
+    print("Calculating the Moon Landing Year in the Quran")
     for chapter in chapters:
         id = chapter['id']
         verses_count = chapter['verses_count']
         name_simple = chapter['name_simple']
-        print(f"Chapter {id} has {verses_count} verses and its name is {name_simple}")
         if id >= 54:
+            print(f"Chapter {id} has {verses_count} verses and its name is {name_simple}")
             # 54 is the Surah Al-Qamar, the moon
             # started counting verses from the moon chapter & onwards
             total_verses = total_verses + verses_count
     
+    print(lines)
     # if we subtract 5 from 10. The result is 5 but the offset is 4
     # i.e 10-5 = (6,7,8,9)
     # same way we are subtracting 1 from the total verses to get offset 
     # or steps required to go to the 6236th (total verses in the Quran) verse
     verses_offset = total_verses-1    
-    print(f"The year of the moon landing in the Quran is {verses_offset}")
+    print(f"The year of the moon landing in the Quran is: {color.BOLD}{verses_offset}{color.END}")
+    
+    message = "For more information read following article:\nhttps://www.answeringislamicskeptics.com/moon-landing.html"
+    print(message)
+    print(lines)
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Moon Landing Year in the Quran
+
+This Python script calculates the year of the moon landing according to the Quran.
+
+## How it works
+
+The script reads a JSON file named `chapters.json` which contains information about the chapters of the Quran. Each chapter in the JSON file has an `id`, `verses_count`, and `name_simple`.
+
+The script then iterates over each chapter. If the `id` of the chapter is 54 or more, it starts counting the number of verses. The number 54 is significant because it corresponds to Surah Al-Qamar, which is associated with the moon.
+
+The total number of verses from Surah Al-Qamar onwards is then calculated. This total is then decremented by 1 to get the offset or the steps required to go to the 6236th verse (which is the total number of verses in the Quran).
+
+The script then prints the calculated year of the moon landing according to the Quran.
+
+## Usage
+
+To run the script, simply execute the `main.py` file:
+
+```bash
+python main.py
+```
+Make sure you have the `chapters.json` file in the same directory as the script.
+
+## Requirements
+- Python 3
+- JSON file named `chapters.json` in the same directory as the script
